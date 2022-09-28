9674ffba99b0b7237208e55cf9b9b44cd70eec2f
Yusuf Boran <yusuf.boran@rbbt.com.tr>
2022-09-28 11:35:30 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import './App.module.scss';
-import BaseLayout from "./screens/BaseLayout";
-import {BrowserRouter} from "react-router-dom";
+import React, { useState } from "react";
+import Navbar from "./layouts/navbar/Navbar";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Portfolio from "./pages/portfolio/Portfolio";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Box, Grid } from "@mui/material";
+import Style from "./App.module.scss";
 
-function App() {
-   return (
-      <div>
-         <BrowserRouter>
-            <BaseLayout/>
-         </BrowserRouter>
-      </div>
-   );
+export default function App() {
+  let [darkMode, setDarkMode] = useState(false);
+
+  function handleClick() {
+    setDarkMode(!darkMode);
+  }
+
+  return (
+    <BrowserRouter>
+      <Box className={darkMode ? Style.dark : Style.light}>
+        <Grid
+          container
+          display={"flex"}
+          flexDirection={"column"}
+          minHeight={"100vh"}
+          justifyContent={"space-between"}
+        >
+          <Grid item>
+            <Navbar darkMode={darkMode} handleClick={handleClick} />
+          </Grid>
+          <Grid item flexGrow={1}>
+            <Routes>
+              <Route exact index path={"/"} element={<Home />} />
+              <Route exact path={"/about"} element={<About />} />
+              <Route exact path={"/portfolio"} element={<Portfolio />} />
+            </Routes>
+          </Grid>
+          
+        </Grid>
+      </Box>
+    </BrowserRouter>
+  );
 }
-
-
-export default App;
