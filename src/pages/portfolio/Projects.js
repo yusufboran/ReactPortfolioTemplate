e475e558f143bc756cf4178a970a5b051ee9f3c8
Yusuf Boran <yusuf.boran@rbbt.com.tr>
2022-12-29 14:47:29 +0300

    project update

--- src/pages/portfolio/Projects.js
+++ src/pages/portfolio/Projects.js
@@ -1,19 +1,10 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Button from "@mui/material/Button";
-import CameraIcon from "@mui/icons-material/PhotoCamera";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import Link from "@mui/material/Link";
 
 const cards = [
   {
@@ -34,9 +25,9 @@ const cards = [
   },
   {
     id: 1,
-    title: "Deneme 2",
+    title: "MielProje",
     imgUrl:
-      "https://raw.githubusercontent.com/yusufboran/dasboard-project/main/src/screenshots/home.jpg",
+    "https://raw.githubusercontent.com/yusufboran/ReactPortfolioTemplate/master/src/assets/img/projectImage/Miel.png",
     context:
       "Laborum do eiusmod ut enim est.Nulla cillum mollit consectetur do non amet ullamco aute cupidatat adipisicing consequat.",
   },
